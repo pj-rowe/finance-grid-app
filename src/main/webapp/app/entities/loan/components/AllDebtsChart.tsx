55caf6b35c21ab5dacdbc5b40cd81ee618c0e677
PJ <rowe.peterjohn@gmail.com>
2020-11-07 11:46:51 -0700

    updated icon for debt labels

--- src/main/webapp/app/entities/loan/components/AllDebtsChart.tsx
+++ src/main/webapp/app/entities/loan/components/AllDebtsChart.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Grid, Button, Typography, makeStyles, Theme, createStyles } from '@material-ui/core';
-import { GoPrimitiveDot as DotIcon } from 'react-icons/all';
+import { BiCheckboxSquare as DotIcon } from 'react-icons/all';
 
 const debtLabelBtnStyles = makeStyles((theme: Theme) =>
   createStyles({
