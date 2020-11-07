3a93003a6cff1495b4f733a7d6d780f5ae206ba3
PJ <rowe.peterjohn@gmail.com>
2020-11-07 11:33:09 -0700

    style debt label buttons properly

--- src/main/webapp/app/entities/loan/components/AllDebtsChart.tsx
+++ src/main/webapp/app/entities/loan/components/AllDebtsChart.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import { Grid, Button, Typography } from '@material-ui/core';
+import { Grid, Button, Typography, makeStyles, Theme, createStyles } from '@material-ui/core';
 import { GoPrimitiveDot as DotIcon } from 'react-icons/all';
 
+const debtLabelBtnStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    debtLabelBtn: {
+      backgroundColor: 'inherit',
+      color: (props: { customColor: string }) => {
+        return props.customColor
+      },
+      borderColor: (props: { borderColor: string }) => {
+        return props.borderColor
+      },
+      '&:hover': {
+        color: '#fff',
+        backgroundColor: (props: { customColor: string }) => {
+          return props.customColor
+        },
+        borderColor: (props: { borderColor: string }) => {
+          return props.borderColor
+        },
+      },
+      '&:active': {
+        borderColor: (props: { borderColor: string }) => {
+          return props.borderColor
+        },
+      },
+      '&:focus': {
+        outline: 'none'
+      }
+    }
+  })
+)
 
 const AllDebtsChart = ({ loanList }) => {
 
@@ -13,9 +43,9 @@ const AllDebtsChart = ({ loanList }) => {
 
   const datasets = loanList.slice()
     .map( loan => {
-      const red = Math.round(Math.random()*255);
-      const blue = Math.round(Math.random()*255);
-      const green = Math.round(Math.random()*255);
+      const red = Math.round(Math.random()*192);
+      const blue = Math.round(Math.random()*192);
+      const green = Math.round(Math.random()*192);
 
       return ({
         label: loan.name,
@@ -48,13 +78,15 @@ const AllDebtsChart = ({ loanList }) => {
     },
   };
 
+  const styles = datasets.map(d => debtLabelBtnStyles({ customColor: d.backgroundColor, borderColor: d.borderColor }))
+
   return (
     <div style={{ marginBottom: '5rem', marginTop: '5rem' }}>
-      <Grid container justify="center" spacing={1}>
+      <Grid container justify="center" spacing={1} style={{ marginBottom: '1rem' }}>
         {
-          datasets.map(d => (
+          datasets.map((d, i) => (
             <Grid item>
-              <Button variant="outlined" color="secondary" size="small" startIcon={ <DotIcon />} style={{ color: d.backgroundColor, borderColor: d.borderColor }}>
+              <Button className={ styles[i].debtLabelBtn } variant="outlined" color="secondary" size="small" startIcon={ <DotIcon />}>
                 <Typography variant="body2">{ d.label }</Typography>
               </Button>
             </Grid>
